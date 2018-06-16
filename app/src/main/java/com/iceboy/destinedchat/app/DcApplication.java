ccd83c5a296b487d43fcaba338c0af36480b2534
hncboy <619452863@qq.com>
2018-06-16 13:56:43 +0800

    增加了接收消息的通知和声音

--- app/src/main/java/com/iceboy/destinedchat/app/DcApplication.java
+++ app/src/main/java/com/iceboy/destinedchat/app/DcApplication.java
@@ -2,15 +2,28 @@ package com.iceboy.destinedchat.app;
 
 import android.app.ActivityManager;
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.media.SoundPool;
 import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.hyphenate.EMMessageListener;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMOptions;
+import com.hyphenate.chat.EMTextMessageBody;
+import com.iceboy.destinedchat.R;
+import com.iceboy.destinedchat.adapter.EMMessageListenerAdapter;
 import com.iceboy.destinedchat.database.db.DatabaseManager;
+import com.iceboy.destinedchat.ui.activity.ChatActivity;
 import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
 import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
@@ -37,6 +50,9 @@ public class DcApplication extends Application {
 
     private static final String TAG = "DcApplication";
     private CosXmlService cosXmlService;
+    private SoundPool mSoundPool;
+    private int mCommonSound;
+    private int mEspeciallySound;
 
     @Override
     public void onCreate() {
@@ -46,6 +62,19 @@ public class DcApplication extends Application {
         initCos();
         initImageLoader();
         initDatabase();
+        initSoundPool();
+        EMClient.getInstance().chatManager().addMessageListener(mEMMessageListenerAdapter);
+    }
+
+    /**
+     * 初始化声音
+     */
+    private void initSoundPool() {
+        mSoundPool = new SoundPool.Builder().setMaxStreams(2).build();
+        //普通的声音
+        mCommonSound = mSoundPool.load(this, R.raw.common, 1);
+        //特别提示音
+        mEspeciallySound = mSoundPool.load(this, R.raw.especially, 1);
     }
 
     /**
@@ -144,11 +173,9 @@ public class DcApplication extends Application {
 
         if (processAppName == null || !processAppName.equalsIgnoreCase(getPackageName())) {
             Log.e(TAG, "enter the service process!");
-            System.out.println("----------------------------------");
             // 则此application::onCreate 是被service 调用的，直接返回
             return;
         }
-        System.out.println("===============================");
         //初始化
         EMClient.getInstance().init(getApplicationContext(), options);
         //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
@@ -180,5 +207,83 @@ public class DcApplication extends Application {
         }
         Log.e(TAG, "getAppName: " + processName);
         return processName;
+    }
+
+    /**
+     * 收到消息的监听
+     */
+    private EMMessageListenerAdapter mEMMessageListenerAdapter = new EMMessageListenerAdapter() {
+
+        @Override
+        public void onMessageReceived(List<EMMessage> list) {
+            if (isForeground()) {
+                mSoundPool.play(mCommonSound, 1, 1, 0, 0, 1);
+            } else {
+                mSoundPool.play(mEspeciallySound, 1, 1, 0, 0, 1);
+                showNotification(list.get(0));
+            }
+        }
+    };
+
+    /**
+     * 如果app不再前台则显示通知
+     *
+     * @param emMessage
+     */
+    private void showNotification(EMMessage emMessage) {
+        String contentText = "";
+        if (emMessage.getBody() instanceof EMTextMessageBody) {
+            contentText = ((EMTextMessageBody) emMessage.getBody()).getMessage();
+        }
+
+        Intent chat = new Intent(this, ChatActivity.class);
+        chat.putExtra(Constant.Extra.USERNAME, emMessage.getUserName());
+        //用TaskStackBuilder来获取PendingIntent处理点击跳转到别的Activity
+        PendingIntent pendingIntent = TaskStackBuilder.create(this)
+                .addParentStack(ChatActivity.class)
+                .addNextIntent(chat)
+                // FLAG_CANCEL_CURRENT：如果要创建的PendingIntent已经存在了，
+                // 那么在创建新的PendingIntent之前，原先已经存在的PendingIntent中的intent将不能使用
+                .getPendingIntent(1, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        //获取NotificationManager的实例
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        //定义Notification的各种属性
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.destinedchat)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_round))
+                .setContentTitle(getString(R.string.receive_new_message))
+                .setContentText(contentText)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true) //自己维护通知的消失
+                .build();
+
+        //设置震动和呼吸灯亮
+        notification.defaults = Notification.DEFAULT_ALL;
+        //发出通知
+        notificationManager.notify(1, notification);
+    }
+
+    /**
+     * 判断app是否在前台
+     *
+     * @return
+     */
+    public boolean isForeground() {
+        //获取系统服务的ActivityManager
+        ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        //获得系统运行的进程
+        List<ActivityManager.RunningAppProcessInfo> runningAppProcessInfos = am.getRunningAppProcesses();
+        if (runningAppProcessInfos == null) {
+            return false;
+        }
+        //遍历所有进程，判断当前应用是否在前台,也就是说是否在屏幕顶部，用户正在进行交互
+        for (ActivityManager.RunningAppProcessInfo info : runningAppProcessInfos) {
+            if (info.processName.equals(getPackageName()) && info.importance ==
+                    ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                return true;
+            }
+        }
+        return false;
     }
 }
